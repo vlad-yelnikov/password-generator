0c85d2caed4e7624218f8ad323ffe8f1871e8c40
vlad-yelnikov <vlad.yelnikov@gmail.com>
2019-10-28 00:25:45 +0200

    added pasword generation

--- script.js
+++ script.js
@@ -1,3 +1,80 @@
-function getRandomLower() {
+const resultEl = document.getElementById('result');
+const lengthEl = document.getElementById('length');
+const uppercaseEl = document.getElementById('uppercase');
+const lowercaseEl = document.getElementById('lowercase');
+const numbersEl = document.getElementById('numbers');
+const symbolsEl = document.getElementById('symbols');
+const generateEl = document.getElementById('generate');
+const clipboardEl = document.getElementById('clipboard');
+
+const randomFunc = {
+    lower: getRandomLower,
+    upper: getRandomUpper,
+    number: getRandomNumber,
+    symbol: getRandomSymbol
+};
+
+generateEl.addEventListener('click', () => {
+    const length = +lengthEl.value;
+    const hasLower = lowercaseEl.checked;
+    const hasUpper = uppercaseEl.checked;
+    const hasNumber = numbersEl.checked;
+    const hasSymbol = symbolsEl.checked;
+
+    resultEl.innerText = generatePassword(
+        hasLower,
+        hasUpper,
+        hasNumber,
+        hasSymbol,
+        length
+    );
+});
+
+function generatePassword(lower, upper, number, symbol, length) {
     
+    //1. Init password variable
+    let generatedPassword = '';
+
+    const typesCount = lower + upper + number + symbol;
+
+    //2. Filter out unchecked types
+    const typesArr = [{ lower }, { upper }, { number }, { symbol }].filter(
+        item => Object.values(item)[0]
+    );
+
+    if (typesCount === 0) {
+        return '';
+    }
+
+    //3. Loop over length call generator function for each type
+    for (let i = 0; i < length; i += typesCount) {
+        typesArr.forEach(type => {
+            const funcName = Object.keys(type)[0];
+            // console.log('funcName: ', funcName);
+
+            generatedPassword += randomFunc[funcName]();
+        });
+    }
+
+    //4. Add final password to the password variable and return
+    const finalPassword = generatedPassword.slice(0, length);
+
+    return finalPassword;
+}
+
+function getRandomLower() {
+    return String.fromCharCode(Math.floor(Math.random() * 26) + 97);
+}
+
+function getRandomUpper() {
+    return String.fromCharCode(Math.floor(Math.random() * 26) + 65);
+}
+
+function getRandomNumber() {
+    return String.fromCharCode(Math.floor(Math.random() * 10) + 48);
+}
+
+function getRandomSymbol() {
+    const symbols = '!@#$%^&*()_';
+    return symbols[Math.floor(Math.random() * symbols.length)];
 }
